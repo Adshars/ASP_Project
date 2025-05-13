426aadfbc9c702ff9d5bffdbe9e072855b1f3deb
Adshars <165464514+Adshars@users.noreply.github.com>
2025-05-13 20:47:26 +0200

    Update Program.cs

--- Warehouse/Program.cs
+++ Warehouse/Program.cs
@@ -45,6 +45,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<JwtService>();
+
 
 //CORS
 builder.Services.AddCors(options =>
